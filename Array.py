e5301f102b82496869fbaea764271ab467ed7c75
Jibril Arbicho <jibrilarbicho2@gmail.com>
2024-04-21 20:26:40 +0300

    Python  DSA

---
+++ Array.py
@@ -0,0 +1,7 @@
+from array import array
+
+my_array = array("i", [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+for i in my_array:
+    print(i)
+my_array.reverse()
+# print(my_array)  # This line is commented out
