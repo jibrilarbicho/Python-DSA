3ec2552342a4ef47f2cc55355658ea83c54e45ff
Jibril Arbicho <jibrilarbicho2@gmail.com>
2024-04-21 21:12:37 +0300

    Insertion to array

--- Array.py
+++ Array.py
@@ -1,7 +1,8 @@
-from array import array
-
-my_array = array("i", [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+import array
+my_array = array.array("i",[1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 for i in my_array:
     print(i)
+my_array.insert(2,50)
+print(my_array)
 my_array.reverse()
 # print(my_array)  # This line is commented out
