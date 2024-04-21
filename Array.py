f8effa967bc3667f10b77a80a22e32e131e09242
Jibril Arbicho <jibrilarbicho2@gmail.com>
2024-04-21 21:54:47 +0300

    Accessing an Element of Array

--- Array.py
+++ Array.py
@@ -1,8 +1,16 @@
 import array
 my_array = array.array("i",[1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-for i in my_array:
-    print(i)
+
+# for i in my_array:
+#     print(i)
 my_array.insert(2,50)
-print(my_array)
-my_array.reverse()
+# print(my_array)
+# my_array.reverse()
 # print(my_array)  # This line is commented out
+#Accessing an element of Array
+def AccessElement(array,index):
+    if index>len(array):
+        print("Index out of range")
+    else:
+        print(array[index])
+AccessElement(my_array,2)
