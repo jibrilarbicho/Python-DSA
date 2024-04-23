878f69da4bd71fc9547f2409b7be184e78a66ed9
Jibril Arbicho <jibrilarbicho2@gmail.com>
2024-04-23 10:04:17 +0300

    Permutation

---
+++ ArrayList Coding Challenge/permutation.py
@@ -0,0 +1,12 @@
+def permutation(list1,list2):
+    if len(list1)!=len(list2):
+        return False
+    list1.sort()
+    list2.sort()
+    if(list1==list2):
+        return True
+    else  :
+      return False
+list1=[1,2,3,4]
+list2=[2,1,4,3]
+print(permutation(list1,list2))
