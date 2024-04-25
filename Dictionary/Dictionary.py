047ffae5b69a453c77d228b2e3eb6945ef775c54
Jibril Arbicho <jibrilarbicho2@gmail.com>
2024-04-25 13:52:12 +0300

    Inserting element at begining of the linkedlist

--- Dictionary/Dictionary.py
+++ Dictionary/Dictionary.py
@@ -3,3 +3,12 @@ eng_sp_dict=dict(eng_sp_list)
 print(eng_sp_dict)
 newdict={}.fromkeys([1,2,3,4,5],2)
 print(newdict)
+myDict={
+    1:"one",
+    2:"two",
+    3:"three",
+    4:"four",
+    5:"five"
+}
+print(1 in myDict)
+print("five" in myDict.values()) # to check values in dictionary
