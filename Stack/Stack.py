85b13a3667e5b486ae0df08cdc2fe88040ac2e02
Jibril Arbicho <jibrilarbicho2@gmail.com>
2024-05-02 23:22:13 +0300

    Operation on Stack using List

--- Stack/Stack.py
+++ Stack/Stack.py
@@ -2,6 +2,41 @@ class Stack:
     def __init__(self):
         self.list = []
     def __str__(self) :
-        values=self.list.reverse()
-        value=[str(x) for x in values ]
-        return ' -> '.join(value)
+        self.list.reverse()
+        value=[str(x) for x in self.list ]
+        # reversed_list = list(reversed(self.list))  # Reverse the list
+        # value = [str(x) for x in reversed_list]  
+        return ' \n'.join(value)
+    def IsEmpty(self) :
+        if self.list==[] : 
+            return True
+        else :
+            return False
+    def push(self, value) :
+        self.list.append(value)
+        return "The element has been successfuly pushed"
+    def pop(self) :
+        if self.list==[] :
+            return "The stack is empty"
+        else :
+            return self.list.pop()
+        
+    def peek(self) :
+        if self.list==[] :
+            return "The stack is empty"
+        else :
+            return self.list[len(self.list)-1]
+        
+
+customStack=Stack()
+customStack.push(1)
+
+print(customStack.IsEmpty())
+customStack.push(2)
+customStack.push(3)
+customStack.push(4)
+# print(customStack)
+# print(customStack.pop())
+# print(customStack)
+print(customStack.peek())
+
