cdbcf326573c36e49237ee01e4e138d9908e6a0c
Jibril Arbicho <jibrilarbicho2@gmail.com>
2024-05-13 03:00:57 +0300

    Fibonacci Number Using  Recursion

--- Recursion/factoial.py
+++ Recursion/factoial.py
@@ -5,3 +5,11 @@ def factorial(n):
     else:
         return n * factorial(n-1)
 print(factorial(32))
+
+def fibonacci(n):
+    assert n>=0 and int(n)==n ,"The number must be integer"
+    if n in [0,1]:
+        return n
+    else:
+        return fibonacci(n-1)+fibonacci(n-2)
+print(fibonacci(4))
