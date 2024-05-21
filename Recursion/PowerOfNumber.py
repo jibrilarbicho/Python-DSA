604831332f3e6a2387ff0c243e60f57732e3a5d3
Jibril Arbicho <jibrilarbicho2@gmail.com>
2024-05-21 07:08:38 +0300

    negative  exponent

--- Recursion/PowerOfNumber.py
+++ Recursion/PowerOfNumber.py
@@ -1,10 +1,12 @@
 def power(base,exp):
 
-    assert exp>=0 and int(exp)==exp ,"The number must be integer"
+    assert  int(exp)==exp ,"The number must be integer"
     if exp==0:
         return 1
+    elif exp<0:
+       return 1/base*power(base,exp+1)
     
 
-
-    return base*power(base,exp-1)
-print(power(4,2))
+    else:
+        return base*power(base,exp-1)
+print(power(4,-2))
