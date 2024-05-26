600fc8c9b3c2f8a98556149f90ac9448fa43b7cf
Jibril Arbicho <jibrilarbicho2@gmail.com>
2024-05-26 21:46:40 +0300

    PreOrder traversal binary tree

--- Tree/Binary Tree.py
+++ Tree/Binary Tree.py
@@ -3,5 +3,17 @@ class TreeNode:
        self.data=data
        self.left=None
        self.right=None
+def preOrderTraversal(rootNode):
+        if not rootNode:
+            return
+        print(rootNode.data)
+        preOrderTraversal(rootNode.left)
+        preOrderTraversal(rootNode.right)
+
     
 newBTreeNode = TreeNode("Drinks")
+Leftchild=TreeNode("Hot")
+Rightchild=TreeNode("Cold")
+newBTreeNode.left=Leftchild
+newBTreeNode.right=Rightchild
+preOrderTraversal(newBTreeNode)
