7381eaf57bd195b189c84ed137a63e90283f94d1
Jibril Arbicho <jibrilarbicho2@gmail.com>
2025-02-14 11:18:36 +0300

    inserting only unique elements to minnodes

--- Stack/Stack Minimum.py
+++ Stack/Stack Minimum.py
@@ -1,27 +1,41 @@
 class Node:
-    def __init__(self, value):
+    def __init__(self, value=None,next=None):
         self.value = value
-        self.next = None
+        self.next = next
 class Stack():
     def __init__(self):
         self.head = None
-        self.minimum=None
+        self.minimumNode=None
+    def min(self):
+        if self.minimumNode is None:
+            return None
+        else:
+            return self.minimumNode.value
 
     def push(self, value):
-        new_node = Node(value)
+        if not self.minimumNode or (value<self.minimumNode.value):
+            self.minimumNode=Node(value=value,next=self.minimumNode)
+        
+        self.head=Node(value=value,next=self.head)
+    def pop(self):
         if self.head is None:
-            self.head = new_node
-            self.minimum = self.head
-
+            return None
         else:
-            if value < self.minimum.value:
+            self.minimumNode=self.minimumNode.next
+            value=self.head.value
+            self.head=self.head.next
+            return value
+    def minnOdes(self):
+            while self.minimumNode.next!=None:
+                print("min",self.minimumNode.value)
+                self.minimumNode=self.minimumNode.next
+            print("min",self.minimumNode.value)
 
-                self.minimum = new_node
-            new_node.next = self.head
-            self.head = new_node
+            
 stack=Stack()
-stack.push(3)
-stack.push(0)
+stack.push(5)
 stack.push(-200)
-stack.push(1)
-print("Minimum element is", stack.minimum.value)
+stack.push(8)
+
+print("Minimum element is", stack.min())
+stack.minnOdes()
