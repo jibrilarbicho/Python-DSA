43982909dc1e82cdfc98217f2772afd67bad2999
Jibril Arbicho <jibrilarbicho2@gmail.com>
2025-03-02 22:49:46 +0300

    removing vertex

--- Graph/001.py
+++ Graph/001.py
@@ -37,19 +37,36 @@ class Graph:
         for vertex in self.graph_dictionary:
             print(vertex,":",self.graph_dictionary[vertex])
     def removeEdge(self,vertex1,vertex2):
-        if vertex2 not in self.graph_dictionary[vertex1] or vertex1 not in self.graph_dictionary[vertex2]:
+        try:
+            if vertex2 in self.graph_dictionary[vertex1] and vertex1 in self.graph_dictionary[vertex2]:
+                self.graph_dictionary[vertex1].remove(vertex2)
+                self.graph_dictionary[vertex2].remove(vertex1)
+        except ValueError:
             print("Error: Edge does not exist.")
-        self.graph_dictionary[vertex1].remove(vertex2)
-        self.graph_dictionary[vertex2].remove(vertex1)
+    def removevertex(self,vertex):
+        if vertex in self.graph_dictionary:
+            for othervertex in self.graph_dictionary:
+                if vertex in self.graph_dictionary[othervertex]:
+                    self.graph_dictionary[othervertex].remove(vertex)
+            del self.graph_dictionary[vertex]
+        else:
+            print("Error: Vertex does not exist.")
+              
 graph=Graph()
 graph.AddVertex("A")
 graph.AddVertex("B")
 graph.AddVertex("C")
 graph.AddVertex("D")
+graph.AddVertex("E")
+
 graph.Addegde("A","B")
 graph.Addegde("A","C")
 graph.Addegde("A","D")
+graph.Addegde("B","E")
 graph.PrintGraph()
-graph.removeEdge("A","B")
+graph.removeEdge("A","E")
+graph.PrintGraph()
+graph.removevertex("A")
+print("After removing vertex A")
 graph.PrintGraph()
 
